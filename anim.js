7aaa8f6e1badaf3aa9e3340534b3fa5a6294190d
goyyapazham <nalanda99@gmail.com>
2017-02-26 21:06:46 -0500

    final

--- anim.js
+++ anim.js
@@ -5,6 +5,9 @@ var cb = document.getElementById("circle");
 var db = document.getElementById("dvd");
 var sb = document.getElementById("stop");
 
+var dvd = new Image();
+dvd.src = "dvd.png";
+
 ctx.fillStyle = "#428c33";
 
 var rid = 0;
@@ -17,6 +20,7 @@ var animateCircle = function() {
     var x = c.width/2; var y = c.height/2;
 
     var grow = true;
+    var speed = 1; //<------ CHANGE SPEED HERE
 
     var draw = function() {
 
@@ -26,24 +30,10 @@ var animateCircle = function() {
 	ctx.arc( x, y, rad, 0, 2 * Math.PI );
 	ctx.fill();
 
-	if( grow ) {
-	    if( rad == y ) {
-		grow = false; rad--;
-	    }
-	    else {
-		rad++;
-	    }
-	};
-	
-	if( !grow ) {
-	    if( rad == 0 ) {
-		grow = true;
-		rad++;
-	    }
-	    else {
-		rad--;
-	    }
-	};
+	rad += speed;
+
+	if( rad == y || rad == 0 )
+	    speed = -speed;
 
 	console.log(grow);
 	console.log(rad);
@@ -56,10 +46,38 @@ var animateCircle = function() {
 
 };
 
+var animateDVD = function() {
+
+    window.cancelAnimationFrame( rid );
+
+    var x = Math.random() * ( c.width - 150 ) + 75;
+    var y = Math.random() * ( c.height - 90 ) + 45;
+    var xspeed = 1; var yspeed = 1;
+
+    var bounce = function() {
+
+	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.drawImage(dvd, x, y, 50, 50);
+	
+	x += xspeed; y += yspeed;
+
+	if( x + 50 > c.width || x < 0 )
+	    xspeed = -xspeed;
+	if( y + 50 > c.height || y < 0 )
+	    yspeed = -yspeed;
+
+	rid = window.requestAnimationFrame(bounce);
+
+    }
+
+    bounce();
+
+}
+
 var stopAnimation = function() {
     window.cancelAnimationFrame( rid );
 };
 
 cb.addEventListener( "click", animateCircle );
-//db.addEventListener( "click", animateDVD );
+db.addEventListener( "click", animateDVD );
 sb.addEventListener( "click", stopAnimation );
